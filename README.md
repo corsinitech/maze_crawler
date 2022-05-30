c624ec7ef28105e6801b14c847789565e57b5264
Gabe Corsini <gabe@corsini.tech>
2022-05-29 23:40:07 -0400

    Finised product... only took all day

--- README.md
+++ README.md
@@ -6,9 +6,22 @@ Simple command line inputs will be sent to the terminal that allow the player
 to move their icon through maze, with walls they can't cross and deadends.
 
 ### Tasks
-- [ ] Generate main game loop
-- [ ] Generate 2D Array that represents the game map and print to screen
-- [ ] Move player by translating icon to different array points
-- [ ] Prevent player from overriding walls
-- [ ] Check win condition based on final coordinates.
+- [x] Generate main game loop
+- [x] Generate 2D Array that represents the game map and print to screen
+- [x] Move player by translating icon to different array points
+- [x] Prevent player from overriding walls
+- [x] Prevent player from moving outside of map bounds
+- [x] Check win condition based on final coordinates.
 
+### Notes
+So prior to this, I followed a tutorial on how to create a simple tic-tac-toe game
+in java, which set some of the baseline for how this game would be created. I 
+ultimately wanted to try something that was a little more complicated than that,
+relying on the player to remain in bounds, have multiple directions they could go,
+and so on. Some stuff I learned:
+- Better control of 2D arrays
+- Printing 2D arrays out in sequence
+- controlling player inputs with switch cases
+- Providing boundaries for the game board
+- Creating win conditions that can be repeatably checked
+- Using chars and unicode characters for fun
--- src/com/corsinitech/maze.java
+++ src/com/corsinitech/maze.java
@@ -1,30 +1,164 @@
 package com.corsinitech;
 
+import java.util.Scanner;
+import java.awt.Point;
+
+
 public class maze{
+
+    public static final String TEXT_RED = "\u001B[31m";
+    public static final String TEXT_RESET = "\u001B[0m";
+    public static final char playerIcon = '\u2654';
     
    public static void main(String[] args){
+
+       System.out.println(TEXT_RED + """
+ ▄▄       ▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄       ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄            ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ 
+▐░░▌     ▐░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+▐░▌░▌   ▐░▐░▌▐░█▀▀▀▀▀▀▀█░▌ ▀▀▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀      ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌▐░▌          ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌
+▐░▌▐░▌ ▐░▌▐░▌▐░▌       ▐░▌          ▐░▌▐░▌               ▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          ▐░▌          ▐░▌       ▐░▌
+▐░▌ ▐░▐░▌ ▐░▌▐░█▄▄▄▄▄▄▄█░▌ ▄▄▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄      ▐░▌          ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌   ▄   ▐░▌▐░▌          ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌
+▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌  ▐░▌  ▐░▌▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+▐░▌   ▀   ▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀      ▐░▌          ▐░█▀▀▀▀█░█▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░▌ ▐░▌░▌ ▐░▌▐░▌          ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀█░█▀▀ 
+▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          ▐░▌               ▐░▌          ▐░▌     ▐░▌  ▐░▌       ▐░▌▐░▌▐░▌ ▐░▌▐░▌▐░▌          ▐░▌          ▐░▌     ▐░▌  
+▐░▌       ▐░▌▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄      ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌      ▐░▌ ▐░▌       ▐░▌▐░▌░▌   ▐░▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌      ▐░▌ 
+▐░▌       ▐░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░░▌     ▐░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌
+ ▀         ▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀       ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀         ▀  ▀▀       ▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀ 
+                                                                                                                                                   
+               """ + TEXT_RESET);
         char[][] mazeMap = {
-            {'O','O','O','O','O','O','O',' ','O','O',},
-            {'O','o','o','o','o','o',' ',' ','o','O',},
-            {'O','o','o','o','o','o',' ','o','o','O',},
-            {'O','o','o','o','o','o',' ','o','o','O',},
-            {'O','o','o','o','o','o',' ',' ',' ','O',},
-            {'O',' ',' ',' ',' ',' ','o','o',' ','O',},
-            {'O',' ','o','o','o',' ','o','o',' ','O',},
-            {'O',' ',' ','o','o',' ',' ',' ',' ','O',},
-            {'O','o',' ','o','o','o','o','o','o','O',},
-            {'O','O','x','O','O','O','O','O','O','O',},
+            {'O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O',' ','O','O',},
+            {'O','o','o','o','o','o','o','o','o','o','o','o','o','o','o','o','o',' ','o','O',},
+            {'O','o','o','o','o','o','o','o','o','o','o','o',' ',' ',' ',' ','o',' ','o','O',},
+            {'O','o','o','o',' ',' ',' ','o','o','o','o','o',' ','o','o',' ',' ',' ','o','O',},
+            {'O','o','o','o',' ','o',' ','o','o','o','o','o',' ','o','o','o','o','o','o','O',},
+            {'O','o','o','o',' ','o',' ',' ',' ',' ','o','o',' ','o','o','o','o','o','o','O',},
+            {'O','o',' ',' ',' ','o','o','o','o',' ','o','o',' ','o','o','o','o','o','o','O',},
+            {'O','o',' ','o','o','o','o','o','o',' ','o','o',' ','o','o','o','o','o','o','O',},
+            {'O','o',' ','o','o','o','o',' ',' ',' ','o','o',' ',' ',' ','o','o','o','o','O',},
+            {'O','o',' ','o','o','o','o',' ','o','o','o','o','o','o',' ','o','o','o','o','O',},
+            {'O','o',' ','o','o','o','o',' ','o','o','o','o','o','o',' ','o','o','o','o','O',},
+            {'O','o',' ',' ',' ',' ','o',' ',' ',' ',' ',' ',' ',' ',' ','o','o','o','o','O',},
+            {'O','o','o','o','o',' ','o','o','o','o','o','o','o','o','o','o','o','o','o','O',},
+            {'O','o','o','o','o',' ','o','o','o','o','o','o','o','o','o','o','o','o','o','O',},
+            {'O','o','o','o','o',' ','o','o','o','o','o','o','o','o','o','o','o','o','o','O',},
+            {'O','o','o',' ',' ',' ','o','o','o','o','o','o','o','o','o','o','o','o','o','O',},
+            {'O',' ',' ',' ','o','o','o','o','o','o','o','o','o','o','o','o','o','o','o','O',},
+            {'O',' ','o','o','o','o','o','o','o','o','o','o','o','o','o','o','o','o','o','O',},
+            {'O',' ','o','o','o','o','o','o','o','o','o','o','o','o','o','o','o','o','o','O',},
+            {'O',playerIcon,'O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O',},
         };
 
         printMaze(mazeMap);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Need some instructions? (y/n)");
+        String answer = input.next();
+
+        if (answer.toLowerCase().equals("y")){
+            printGameInstructions();
+        } else {
+            System.out.println("Let's begin!");
+        }
+        Point playerPos = findPlayerPosition(mazeMap);
+
+        while(true){
+            System.out.println("Choose a direction (w, a, s, d): ");
+            String playerMove = input.next().toLowerCase();
+            try{
+                movePlayerPosition(playerPos, mazeMap, playerMove);
+            } catch (ArrayIndexOutOfBoundsException badMove){
+                System.out.println("Ya can't go out of bounds...");
+            }
+            printMaze(mazeMap);
+            String result = checkWinCondition(playerPos.x, playerPos.y);
+            if(result.length() > 0){
+               System.out.println(result);
+               break;
+            }
+            System.out.println(result);
+        }
    }
 
-    private static void printMaze(char[][] mazeMap) {
+	private static void printGameInstructions() {
+        System.out.println("""
+                Welcome to Maze Crawler! The objective is simple:
+                1. Direct the chess piece through the maze via the given options:
+                    a. w = UP
+                    b. a = LEFT
+                    c. s = DOWN
+                    b. d = RIGHT
+                2. Make it to the end!
+
+                Pretty simple, right?
+                """);
+}
+
+	private static void printMaze(char[][] mazeMap) {
         for (char[] row : mazeMap){
             for(char c : row){
-                System.out.print(c);
+                System.out.print(" " + c + " ");
             }
             System.out.println();
         }
     } 
+
+    private static Point findPlayerPosition(char[][] mazeMap) {
+        Point playerLocation = new Point();
+        for(int y = 0; y < mazeMap.length; y++){
+            for(int x = 0; x < mazeMap[y].length; x++) {
+                if(mazeMap[y][x] == playerIcon){
+                    playerLocation.setLocation(y,x);
+                }
+            }
+        }
+        return playerLocation;
+    }
+
+    private static void movePlayerPosition(Point player, char[][] mazeMap, String playerMove){
+        switch(playerMove){
+            case "w":
+                if(mazeMap[player.x-1][player.y] == ' '){
+                    mazeMap[player.x][player.y] = ' ';
+                    player.x = player.x - 1;
+                    mazeMap[player.x][player.y] = playerIcon;
+                } else if(mazeMap[player.x-1][player.y] == 'o' || mazeMap[player.x-1][player.y] == 'O'){
+                    System.out.println("Invalid move! There's a wall there!");
+                }
+                break;
+            case "a":
+                if(mazeMap[player.x][player.y-1] == ' '){
+                    mazeMap[player.x][player.y] = ' ';
+                    player.y = player.y - 1;
+                    mazeMap[player.x][player.y] = playerIcon;
+                } else if(mazeMap[player.x][player.y-1] == 'o' || mazeMap[player.x][player.y-1] == 'O'){
+                    System.out.println("Invalid move! There's a wall there!");
+                }
+                break;
+            case "s":
+                if(mazeMap[player.x+1][player.y] == ' '){
+                    mazeMap[player.x][player.y] = ' ';
+                    player.x = player.x + 1;
+                    mazeMap[player.x][player.y] = playerIcon;
+                } else if(mazeMap[player.x+1][player.y] == 'o' || mazeMap[player.x+1][player.y] == 'O'){
+                    System.out.println("Invalid move! There's a wall there!");
+                }
+                break;
+            case "d":
+                if(mazeMap[player.x][player.y+1] == ' '){
+                    mazeMap[player.x][player.y] = ' ';
+                    player.y = player.y + 1;
+                    mazeMap[player.x][player.y] = playerIcon;
+                } else if(mazeMap[player.x][player.y+1] == 'o' || mazeMap[player.x][player.y+1] == 'O'){
+                    System.out.println("Invalid move! There's a wall there!");
+                }
+                break;
+        }
+    }
+
+    private static String checkWinCondition(int xPos, int yPos){
+        if(xPos == 0 && yPos == 17){
+            return "You solved the maze!";
+        }
+        return "";
+    }
 }
